69f1aa796e891444dab3ab305ddf359607f22424
RossTheDev <ross.f.pittser@my.occc.edu>
2020-01-26 18:01:33 -0600

    Added p1 picture text to come later

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 600,
+    width: 1400,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
